1b054cb8939e29c811dfff440b8fbe9abdd25dde
Dmitri Baskakov <dmitri.baskakov.git@gmail.com>
2020-11-20 18:25:22 +0700

    StatusLed to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
 #include "StatusLed.h"
 
+StatusLed statusLed(13);
+
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
+    statusLed.update();
 }
