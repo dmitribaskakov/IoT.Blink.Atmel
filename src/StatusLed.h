6e5b2a1e4903272baa3833c392f0f8699218779b
dmitribaskakov <dmitri.baskakov.git@gmail.com>
2024-09-04 23:12:06 +0300

    моргаем по новому

--- src/StatusLed.h
+++ src/StatusLed.h
@@ -4,9 +4,9 @@
 #include <Arduino.h>
 
 //Описание state machine мигания сигнальным светодиодом
-const uint8_t _StatusLed_state_count = 6;	//Колличество состояний
-const bool _StatusLed_states[_StatusLed_state_count] = {LOW, HIGH, LOW, HIGH, LOW, HIGH};
-const uint16_t _StatusLed_times[_StatusLed_state_count] = {1200, 25, 250, 25, 250, 25};
+const uint8_t _StatusLed_state_count = 8;	//Колличество состояний
+const bool _StatusLed_states[_StatusLed_state_count] = 		{LOW, 	HIGH, 	LOW, 	HIGH, 	LOW, 	HIGH, 	LOW, 	HIGH};
+const uint16_t _StatusLed_times[_StatusLed_state_count] = 	{1000, 	1000, 	300, 	50, 	300, 	50, 	300, 	50};
 
 class StatusLed {
 public:
